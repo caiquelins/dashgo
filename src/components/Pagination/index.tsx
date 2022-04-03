099bd98d23d157592b574ee2f6076b0ba8a46a1a
Caique <caique.c.lins@gmail.com>
2022-04-03 16:26:51 -0300

    Trocando de página

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -49,29 +49,29 @@ export function Pagination({
 
         {currentPage > (1 + siblingsCount) && (
           <>
-            <PaginationItem number={1} />
+            <PaginationItem onPageChange={onPageChange} number={1} />
             {currentPage > (2 + siblingsCount) && (
               <Text color="gray.300" width="8" textAlign="center">...</Text>
-              )}
+            )}
           </>
         )}
 
         {previousPage.length > 0 && previousPage.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         })}
 
-        <PaginationItem number={currentPage} isCurrent />
+        <PaginationItem onPageChange={onPageChange} number={currentPage} isCurrent />
 
         {nextPage.length > 0 && nextPage.map(page => {
-          return <PaginationItem key={page} number={page} />
+          return <PaginationItem onPageChange={onPageChange} key={page} number={page} />
         })}
 
         {(currentPage + siblingsCount) < lastPage && (
           <>
             {(currentPage + 1 + siblingsCount) < lastPage && (
               <Text color="gray.300" width="8" textAlign="center">...</Text>
-              )}
-            <PaginationItem number={lastPage} />
+            )}
+            <PaginationItem onPageChange={onPageChange} number={lastPage} />
           </>
         )}
       </Stack>
